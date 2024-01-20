4bed840a43e3b65978d4ef3521ca0ba27bcdeaaf
FlafyDev <flafyarazi@gmail.com>
2024-01-20 23:59:40 +0200

    nix: fix flake

--- flake.nix
+++ flake.nix
@@ -10,35 +10,34 @@
     self,
     flake-utils,
     nixpkgs,
-  }:
+  }: let
+    version = "0.0.1";
+  in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
-
-      version = "0.0.1";
-    in
-      {
-        packages = {
-          emoji-drawing-web = pkgs.callPackage ./website/package.nix {inherit version;};
-          emoji-drawing-server = pkgs.callPackage ./server/package.nix {inherit version;};
-        };
-        # TODO: Generate dev shell from packages.
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            flutter316
-            sqlite
-            nodejs_20
-          ];
-        };
-      }
-      // {
-        overlays.default = _final: prev: {
-          emoji-drawing-web = prev.callPackage ./website/package.nix {inherit version;};
-          emoji-drawing-server = prev.callPackage ./server/package.nix {inherit version;};
-        };
-        nixosModules = {
-          default = import ./module.nix self;
-        };
-      });
+    in {
+      packages = {
+        emoji-drawing-web = pkgs.callPackage ./website/package.nix {inherit version;};
+        emoji-drawing-server = pkgs.callPackage ./server/package.nix {inherit version;};
+      };
+      # TODO: Generate dev shell from packages.
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          flutter316
+          sqlite
+          nodejs_20
+        ];
+      };
+    })
+    // {
+      overlays.default = _final: prev: {
+        emoji-drawing-web = prev.callPackage ./website/package.nix {inherit version;};
+        emoji-drawing-server = prev.callPackage ./server/package.nix {inherit version;};
+      };
+      nixosModules = {
+        default = import ./module.nix self;
+      };
+    };
 }
